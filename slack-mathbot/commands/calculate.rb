42a044ef1e6a2ce8ba542f73585bbbc7048ea9c3
Kevin Candelaria <kevdashdev@gmail.com>
2017-01-21 12:45:44 +0800

    Fix incorrect solution being sent in response due to user id being parsed too

--- slack-mathbot/commands/calculate.rb
+++ slack-mathbot/commands/calculate.rb
@@ -2,10 +2,11 @@ require 'dentaku'
 module SlackMathbot
   module Commands
     class Calculate < SlackRubyBot::Commands::Base
+      NAME_COMMAND_REGEX = /\A(.)*(calculate){1}\ /
       NUM_OP_REGEX = /[([\d]+[\+\-\*\/]{1}[\d]+)+]/
       OP_REGEX = /[\+\-\*\/]/
       command 'calculate' do |client, data, _match|
-        problem = data.text.scan(NUM_OP_REGEX).join
+        problem = data.text.sub(NAME_COMMAND_REGEX, '').scan(NUM_OP_REGEX).join
         response = if problem[-1] =~ (OP_REGEX)
                      "Error"
                    else
