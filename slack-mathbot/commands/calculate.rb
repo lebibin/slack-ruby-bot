d5064b9ec2d78bdd9ea1da093a7ba82b6e40e984
Kevin Candelaria <kevdashdev@gmail.com>
2017-01-21 12:04:18 +0800

    Use dentaku calculator instead of eval

--- slack-mathbot/commands/calculate.rb
+++ slack-mathbot/commands/calculate.rb
@@ -1,15 +1,15 @@
+require 'dentaku'
 module SlackMathbot
   module Commands
     class Calculate < SlackRubyBot::Commands::Base
       NUM_OP_REGEX = /[([\d]+[\+\-\*\/]{1}[\d]+)+]/
       OP_REGEX = /[\+\-\*\/]/
-
       command 'calculate' do |client, data, _match|
         problem = data.text.scan(NUM_OP_REGEX).join
         response = if problem[-1] =~ (OP_REGEX)
                      "Error"
                    else
-                     eval(problem)
+                     ::Dentaku::Calculator.new.evaluate(problem)
                    end
         client.say channel: data.channel, text: response
       end
