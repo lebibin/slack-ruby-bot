7e056dc46d109c4edd899859238d23085284e427
Kevin Candelaria <kevdashdev@gmail.com>
2017-01-21 13:00:32 +0800

    Add bot help description

--- slack-mathbot/bot.rb
+++ slack-mathbot/bot.rb
@@ -1,4 +1,12 @@
 module SlackMathbot
   class Bot < SlackRubyBot::Bot
+    help do
+      title 'Math Bot'
+      desc 'This bot calculates shit for you.'
+
+      command 'calculate' do
+        desc 'Calculates a mathematical expression for you'
+      end
+    end
   end
 end
