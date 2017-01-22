7b071f319b508a56794cbe5b1800243a6f711b75
Kevin Candelaria <kevdashdev@gmail.com>
2017-01-22 22:06:05 +0800

    Set RACK_ENV to test on spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..'))
+ENV['RACK_ENV'] = 'test'
 require 'simplecov'
 SimpleCov.start
 
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..'))
 require 'slack-ruby-bot/rspec'
 require 'slack-mathbot'
